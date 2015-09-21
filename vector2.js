47db9039e2b1b9cc1c5c2c85afe2dd4cf3a3df1e
jharrison56 <j.harrison@live.com.au>
2015-09-21 19:27:23 +0930

    Updated vector
    Added more functions to vector object

---
+++ vector2.js
@@ -0,0 +1,37 @@
+var Vector2 = function()
+{
+	this.x = 0;
+	this.y = 0;
+}
+
+Vector2.prototype.set = function(x, y)
+{
+	this.x = x;
+	this.y = y;
+}
+
+Vector2.prototype.normalize = function()
+{
+	var magnitude = Math.sqrt((this.x * this.x) + (this.y * this.y));
+	
+	this.x = this.x / magnitude;
+	this.y = this.y / magnitude;
+}
+
+Vector2.prototype.add = function(v2)
+{
+	this.x = this.x + v2.x;
+	this.y = this.y + v2.y;
+}
+
+Vector2.prototype.subtract = function(v2)
+{
+	this.x = this.x - v2.x;
+	this.y = this.y - v2.y;
+}
+
+Vector2.prototype.multiplyScalar(num)
+{
+	this.x = this.x * num;
+	this.y = this.y * num;
+}
